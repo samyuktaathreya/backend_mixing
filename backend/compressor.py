2777b54586df3efcdbcb9064860dd84d2601694d
Samyukta Athreya <athreya.samyukta@gmail.com>
2025-12-02 01:52:19 +0000

    compression successfully implemented!

--- backend/compressor.py
+++ backend/compressor.py
@@ -3,18 +3,26 @@ from audiocomplib import AudioCompressor
 import librosa
 import numpy as np
 from pydub import AudioSegment
+import sys
 
 #convert mp3 to ndarray
 def mp3_to_ndarray(mp3_file_path):
     #sr = None preserves native sampline rate of mp3
     audio_array, sampling_rate = librosa.load(mp3_file_path, sr=None) 
 
-    print(f"Audio loaded successfully. Shape of ndarray: {y.shape}")
+    if audio_array.ndim == 1:
+        audio_array = np.expand_dims(audio_array, axis=0)  # (1, samples)
+
+    print(f"Audio loaded successfully. Shape of ndarray: {audio_array.shape}")
     print(f"Sampling rate: {sampling_rate} Hz")
     return audio_array, sampling_rate
 
 #convert ndarrray to mp3
 def ndarray_to_mp3(audio_data, sample_rate, output_filename):
+    # Convert back to 1D if mono
+    if audio_data.ndim == 2 and audio_data.shape[0] == 1:
+        audio_data = audio_data[0]
+
     # Convert float [-1,1] to int16
     int_samples = (audio_data * 32767).astype(np.int16)
     audio_segment = AudioSegment(
@@ -32,8 +40,8 @@ def compress_mp3(input_signal, output_filename):
 
     # Initialize compressor
     compressor = AudioCompressor(
-        threshold=-10.0,
-        ratio=4.0,
+        threshold=-30.0,
+        ratio=12.0,
         attack_time_ms=1.0,
         release_time_ms=100.0,
         knee_width=3.0,
@@ -47,6 +55,12 @@ def compress_mp3(input_signal, output_filename):
     #convert to mp3
     ndarray_to_mp3(compressed_signal, sampling_rate, output_filename)
 
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: python compressor.py <input_filename> <output_filename>")
+    else:
+        compress_mp3(sys.argv[1], sys.argv[2])
 
 #helpful functions to know:
 
